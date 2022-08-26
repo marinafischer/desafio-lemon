541794ca55b8c34dd140494bc716e0e5fa57528e
marinafischer <marinamirandafischer@gmail.com>
2022-08-26 08:00:23 -0300

    feat: start comands

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const isElegible = require('./validations/validateElegibility');
 const schemaValidade = require('./validations/validateSchema');
-// para verificar os dados chame esta função, em seguida rode o comando npm start
+const {data1, data2} = require("./tests/mocks");
 
 const verifyElegible = (data) => {
   try {
@@ -10,7 +10,11 @@ const verifyElegible = (data) => {
     return error;
   }
 }
+// para verificar os dados chame a função acima ou descomente as linhas 16 e 17
+// em seguida rode o comando npm start
 
+console.log(verifyElegible(data1));
+console.log(verifyElegible(data2)); 
 
 module.exports = verifyElegible
 
